464e79c66a250bf0e4666cceb48713de875c2bd1
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2021-03-29 15:51:43 -0400

    minor

--- launchers/scripts/query_cacher_tfrecord_mp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source launchers/scripts/base.sh
-SCRIPT_PATH=query_cacher_tfrecord_mp.py
-CONFIG_PATH=configs/query_cacher_tfr_configs/remote.json
-run "$SCRIPT_PATH" "$CONFIG_PATH"
