296e1937857278f75247a911115501c5a4751db8
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2021-05-21 18:50:01 -0400

    minor

---
+++ launchers/scripts/mila_generation.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+set -u
+
+#source venv/bin/activate
+
+echo "Running base.sh"
+source launchers/scripts/base.sh
+echo "Done running bash.sh"
+
+SCRIPT_PATH="generation.py"
+CONFIG_PATH="configs/generation_configs/mila.json"
+
+LOG_DIR="$HOME/BUCKET-julesgm-research-v3/generation_run_log/"
+LOG_PATH="$LOG_DIR/$(TZ=":America/New_York" date +"%Y-%m-%d_%T")"
+mkdir -p "$LOG_DIR" || true
+
+echo "Writing logs to '$LOG_PATH'."
+run "$SCRIPT_PATH" "$CONFIG_PATH" "$@" 2>&1 | tee "$LOG_PATH"
