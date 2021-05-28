4cf314457c2d054dce389908105de1e02c6f91f2
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2021-05-27 22:00:11 -0400

    tiny fix

--- generation.py
+++ generation.py
@@ -205,7 +205,7 @@ def make_print_sample():
     """
     for title in titles:
       sample = sample.replace(
-        sample, title, f"\n\n[{title_color} bold]{sample}[/]"
+        title, f"\n\n[{title_color} bold]{sample}[/]"
       )
 
     panel = rich.panel.Panel(
